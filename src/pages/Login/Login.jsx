1af7e8de87843987e11032fcc2624b8704c5dbf8
jakariacst318 <jsjakariacst@gmail.com>
2024-08-09 19:18:05 +0600

    captcha and sweet alert

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,8 +1,107 @@
 
+import { loadCaptchaEnginge, LoadCanvasTemplate, validateCaptcha } from 'react-simple-captcha';
+import login from '../../assets/others/authentication2.png'
+import { useEffect, useRef, useState } from 'react';
+import Swal from 'sweetalert2';
+
 const Login = () => {
+    const captchaRef = useRef(null)
+    const [disable, setDisable] = useState(true)
+    useEffect(() => {
+        loadCaptchaEnginge(6);
+    }, [])
+
+    const handleLogin = event => {
+        event.preventDefault()
+        const form = event.target;
+        const email = form.email.value
+        const password = form.password.value
+        console.log(email, password)
+    }
+
+    const handleValidateCaptcha = () => {
+        const userCaptchaValue = captchaRef.current.value;
+        
+        if (validateCaptcha(userCaptchaValue)) {
+            setDisable(false)
+            const Toast = Swal.mixin({
+                toast: true,
+                position: "top-end",
+                showConfirmButton: false,
+                timer: 3000,
+                timerProgressBar: true,
+                didOpen: (toast) => {
+                    toast.onmouseenter = Swal.stopTimer;
+                    toast.onmouseleave = Swal.resumeTimer;
+                }
+            });
+            Toast.fire({
+                icon: "success",
+                title: "Captcha in successfully"
+            });
+
+        }
+
+        else {
+            const Toast = Swal.mixin({
+                toast: true,
+                position: "top-end",
+                showConfirmButton: false,
+                timer: 3000,
+                timerProgressBar: true,
+                didOpen: (toast) => {
+                    toast.onmouseenter = Swal.stopTimer;
+                    toast.onmouseleave = Swal.resumeTimer;
+                }
+            });
+            Toast.fire({
+                icon: "error",
+                title: "Captcha Does Not Match"
+            });
+        }
+        // captchaRef.current.value = ('')
+    }
+
     return (
-        <div>
-            <h2>please login</h2>
+        <div className="hero bg-base-200 min-h-screen">
+            <div className="hero-content flex-col md:flex-row">
+                <div className="text-center lg:text-left">
+
+                    <img src={login} alt="" />
+                </div>
+                <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+                    <h1 className="text-5xl font-bold pb-3 pt-2 text-center">Login</h1>
+                    <form onSubmit={handleLogin} className="card-body">
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text font-semibold">Email</span>
+                            </label>
+                            <input type="email" name='email' placeholder="email" className="input input-bordered" required />
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text font-semibold">Password</span>
+                            </label>
+                            <input type="password" name='password' placeholder="password" className="input input-bordered" required />
+                            <label className="label">
+                                <a href="#" className="label-text-alt font-semibold link link-hover">Forgot password?</a>
+                            </label>
+                        </div>
+                        <div className="form-control font-semibold">
+                            <label className="label">
+                                <LoadCanvasTemplate />
+                            </label>
+                            <input type="text" ref={captchaRef} name='captcha' placeholder="type the captcha" className="input input-bordered"  />
+
+                            <button onClick={handleValidateCaptcha} className="btn btn-outline btn-sm mt-3">Validate Captcha</button>
+
+                        </div>
+                        <div className="form-control mt-6">
+                            <input disabled={disable} className='btn bg-[#D1A054] text-white hover:bg-[#1F2937]' type="submit" value="Login" />
+                        </div>
+                    </form>
+                </div>
+            </div>
         </div>
     );
 };
